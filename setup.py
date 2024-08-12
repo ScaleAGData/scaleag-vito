cc1c50a0f7c66c99a079928eb9217d2e02bb314b
Giorgia Milli <giorgia.milli@vito.be>
2024-08-12 10:42:14 +0200

    fixing folder structure for installation

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages("src"),
-    package_data={"": ["scaleagdata/*", "scaleagdata/*/*"]},
     install_requires=[
         "scikit-learn",
         "scikit-image",
