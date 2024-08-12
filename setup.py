2a47bf7c70efc58762bc2b485a85078eadd65df6
Giorgia Milli <giorgia.milli@vito.be>
2024-08-12 10:01:50 +0200

    setup moved

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import find_packages, setup
 
+from src.scaleagdata.presto import __version__
+
 setup(
     name="scaleagdata_vito",
-    version="0.1",
-    packages=find_packages(where="."),
-    package_dir={"": "."},
+    version=f"{__version__}",
+    packages=find_packages(),
     install_requires=[
         "git+https://github.com/WorldCereal/presto-worldcereal.git",
+        "scikit-learn",
+        "scikit-image",
+        "matplotlib",
     ],
+    zip_safe=True,
     author="Giorgia Milli",
     author_email="giorgia.milli@vito.be",
     description="using Presto for few-shot learning downstream tasks",
