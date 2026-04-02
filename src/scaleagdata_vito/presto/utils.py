e70842017906de6d743a9b333386f23d24c99f36
giollimirgia <milli.giorgia@gmail.com>
2026-04-02 17:14:07 +0200

    application of mask on predictions corrected. evaluation returnd probs in binary case

--- src/scaleagdata_vito/presto/utils.py
+++ src/scaleagdata_vito/presto/utils.py
@@ -118,8 +118,8 @@ def evaluate_finetuned_model(
     # predict with Presto head and evaluate depending on the task
     preds, targets = predict_with_head(test_dl, finetuned_model)
     if test_ds.task_type == "binary":
-        preds = (preds > 0.5).astype(int)
-        metrics = classification_report(targets, preds, output_dict=True)
+        preds_bin = (preds > 0.5).astype(int)
+        metrics = classification_report(targets, preds_bin, output_dict=True)
     elif test_ds.task_type == "multiclass":
         preds = [test_ds.index_to_class[int(t)] for t in preds]
         targets = [test_ds.index_to_class[int(t)] for t in targets]
